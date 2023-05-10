bfe7c557fa96a998d38dfb8942158c352a214245
yuyunchao <294733692@qq.com>
2023-05-10 21:46:54 +0800

    vue-echarts v-charts 插件优化

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <ve-line :data="data"></ve-line>
     <router-view/>
   </div>
 </template>
@@ -7,12 +8,26 @@
 <script>
 
 export default {
-  name: 'app'
+  name: 'app',
+  data () {
+    return {
+      data: {
+        columns: ['日期', '销售额'],
+        rows: [
+          { 日期: '1月1日', 销售额: 123 },
+          { 日期: '1月2日', 销售额: 1234 },
+          { 日期: '1月3日', 销售额: 1243 },
+          { 日期: '1月4日', 销售额: 12413 },
+          { 日期: '1月5日', 销售额: 1223 }
+        ]
+      }
+    }
+  }
 }
 </script>
 
 <style>
-html, body ,#app {
+html, body, #app {
   width: 100%;
   height: 100%;
   padding: 0;
--- src/main.js
+++ src/main.js
@@ -2,10 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+import './plugins/vcharts.js'
 import * as ECharts from 'echarts'
+import VueEcharts from 'vue-echarts'
 
 Vue.config.productionTip = false
 Vue.prototype.$echarts = ECharts
+Vue.component('v-charts', VueEcharts)
+
 new Vue({
   router,
   render: h => h(App)
